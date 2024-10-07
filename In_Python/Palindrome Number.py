96f855b9475bb132e521f4dd5d91b3f54d96452f
Fady Goda <fadygoda@192.168.1.28>
2024-10-07 15:37:07 +0300

    Better Solution

--- In_Python/Palindrome Number.py
+++ In_Python/Palindrome Number.py
@@ -1,14 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, x):
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         """
         :type x: int
         :rtype: bool
         """
 
-        x_reverse = "".join(reversed(str(x)))
-
-        if (x_reverse == str(x)):
-            return True
-        else:
+        if x < 0 or (x != 0 and x % 10 == 0):
             return False
-        
+
+        reversed_number = 0
+
+        while x > reversed_number:
+            reversed_number = reversed_number * 10 + x % 10
+            x //= 10
+
+        return x == reversed_number or x == reversed_number // 10
