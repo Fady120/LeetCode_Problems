ce63cefae52b08d8f80aa67f3dada52bdbee2a84
Fady Goda <fadygoda@192.168.1.9>
2024-10-10 16:25:41 +0300

    Better Solution

--- In_Python/Longest Common Prefix.py
+++ In_Python/Longest Common Prefix.py
@@ -1,21 +1,20 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         """
         :type strs: List[str]
         :rtype: str
         """
-        def compare(strs, first_strs, char, LCP):
-            for value in strs[1:]:
-                if( char < len(value) and char < len(first_strs) ):
-                    if(first_strs[char] != value[char]):
-                        return LCP
-                else: 
-                    return LCP
-            LCP += first_strs[char]
-            char += 1
-            return compare(strs, first_strs, char, LCP)
-        if(len(strs) == 1):
-            return strs[0]
-        else:
-            return compare(strs, strs[0], 0, '')
         
+        strs = sorted(strs)
+        
+        first = strs[0]
+        last = strs[-1]
+        ans = ""
+
+        for i in range(min(len(first),len(last))):
+            if first[i] != last[i]:
+                return ans
+            else:
+                ans += first[i]
+        
+        return ans
